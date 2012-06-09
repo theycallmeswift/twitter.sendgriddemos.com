19dbede4cacb2be4000d207ced5132b16fafa7f4
Mike Swift <theycallmeswift@gmail.com>
2012-06-08 23:43:37 -0400

    Initial commit.

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express')
+  , app = express.createServer()
+  , io = require('socket.io').listen(app)
+  , port = process.env.PORT || 3000
+  , util = require('util')
+  , SendGrid = require('sendgrid').SendGrid
+  , sendgrid = new SendGrid("hackmtl", "hackmtl");
+
+io.set('log level', 0);
+
+app.configure(function() {
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.post('/email', function(req, res) {
+  util.log("Recieved Email from " + req.body.from);
+  io.sockets.emit('post', req.body);
+  res.end();
+});
+
+app.on('listening', function() {
+  console.log("I am listening on " + port);
+});
+
+app.listen(port);
